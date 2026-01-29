bbae2c154e6d5cb59bec0936684e42f9851fb9c9
GitHub Action <action@github.com>
2026-01-29 11:00:07 +0000

    chore: auto-sync JIRA data [2026-01-29T11:00:07Z]

--- client/src/data/jiraMetrics.ts
+++ client/src/data/jiraMetrics.ts
@@ -45,7 +45,7 @@ export interface JiraMetrics {
 }
 
 export const jiraMetrics: JiraMetrics = {
-  lastUpdated: "January 29, 2026 at 3:41 AM MT",
+  lastUpdated: "January 29, 2026 at 4:00 AM MT",
   extractionSource: "JIRA API - 2026-S2",
 
   sprintInfo: {
